9c5a0bb45a8a0f4b8f2083e1d4ce7d0b5df702f5
IvanRomero03 <i.wells.ar@gmail.com>
2023-05-17 16:10:07 -0600

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -6,10 +6,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const inter = Inter({ subsets: ["latin"] });
 const queryClient = new QueryClient();
 
-export const metadata = {
-  title: "Demo Contador Carros",
-  description: "by: Ivan",
-};
+// export const metadata = {
+//   title: "Demo Contador Carros",
+//   description: "by: Ivan",
+// };
 
 export default function RootLayout({
   children,
