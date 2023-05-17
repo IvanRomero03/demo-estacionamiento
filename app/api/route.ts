ef57be24b6edb1031c78a9decf0df9b610680b0d
IvanRomero03 <i.wells.ar@gmail.com>
2023-05-17 16:15:47 -0600

    .

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { redis } from "../redis_client";
 
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+export const GET = async (req: Request) => {
   const count = await redis.get("counter");
   console.log({ count });
   return new Response(String(count), { status: 200 });
-  res.status(200).json({ count });
 };
 
 // export default GET;
